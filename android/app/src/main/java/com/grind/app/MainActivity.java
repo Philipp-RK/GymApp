19d2b7cebf50c270634c9d339c9ca214b01ebea8
Phil <phliptry@gmail.com>
2026-05-14 13:03:02 +0200

    fix

--- android/app/src/main/java/com/grind/app/MainActivity.java
+++ android/app/src/main/java/com/grind/app/MainActivity.java
@@ -1,5 +1,14 @@
 package com.grind.app;
 
 import com.getcapacitor.BridgeActivity;
+import android.os.Bundle;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.bridge.getWebView().getSettings().setUserAgentString(
+            "Mozilla/5.0 (Linux; Android 10; Pixel 4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36"
+        );
+    }
+}
